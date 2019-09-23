99c6384afc5bce2029f1cbc3081287a1c1ca210c
BuzatuCos <allkindcreation@gmail.com>
2019-09-23 15:33:46 +0100

    Fetch the data from DB

--- index.php
+++ index.php
@@ -4,3 +4,12 @@ function dbConn(){
     $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
     return $db;
 }
+function getCarsFromDb($db){
+    $query = $db->prepare("SELECT `Brand`,`Model`,`Year` FROM `Cars`;");
+    $query->execute();
+    $result = $query->fetchAll();
+    return $result;
+}
+$db = dbConn();
+$result = getCarsFromDb($db);
+//var_dump($result);
