9286c53ed38b508e3bd1f76b178af772b9ad70a0
BuzatuCos <allkindcreation@gmail.com>
2019-09-24 09:45:09 +0100

    Add doc blocks

--- index.php
+++ index.php
@@ -1,9 +1,13 @@
 <?php
+//create a function to connect to the database
+//add attribute that will allow to extract only associative arrays
 function dbConn(){
     $db = new PDO('mysql:host=db; dbname=cosminCollection', 'root', 'password');
     $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
     return $db;
 }
+//create a function to fetch data from database
+//after fetching the data we will obtain a multidimensional array with associative arrays
 function getCarsFromDb($db){
     $query = $db->prepare("SELECT `Brand`,`Model`,`Year` FROM `Cars`;");
     $query->execute();
@@ -12,4 +16,5 @@ function getCarsFromDb($db){
 }
 $db = dbConn();
 $result = getCarsFromDb($db);
-//var_dump($result);
+
+
