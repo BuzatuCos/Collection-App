4a421aec849b55dce43ada708ab065011720563c
BuzatuCos <allkindcreation@gmail.com>
2019-09-25 09:22:33 +0100

    adding normalize and indentation

--- index.php
+++ index.php
@@ -8,14 +8,15 @@ $output = extractDataForOutput($result);
 <html lang="en-gb">
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="normalize.css" />
     <link rel="stylesheet" type="text/css" href="styles.css" />
     <title>Collection</title>
 </head>
 <body>
-<h1>Car Collection</h1>
-<section class="section">
-<?php echo $output; ?>
-</section>
+    <h1>Car Collection</h1>
+    <section class="section">
+        <?php echo $output; ?>
+    </section>
 </body>
 </html>
 
