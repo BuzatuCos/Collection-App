41bec81842195543f2acd8ee69226e68d9e4b5c1
BuzatuCos <allkindcreation@gmail.com>
2019-09-26 15:27:20 +0100

    inserting finish

--- insertToDb.php
+++ insertToDb.php
@@ -1,17 +1,27 @@
 <?php
-$newBrand = '';
-$newModel = '';
-$newYear = '';
-if (!empty($_POST['Brand']) && strlen( $_POST[ 'Brand' ] ) <= 255 ) && is_string($_POST[ 'Brand' ] ) {
-    $newBrand =  validation($_POST['Brand']);
-}
-if (!empty($_POST['Model'])&& strlen( $_POST[ 'Model' ] ) <= 255 ) && is_string($_POST[ 'Model' ] )) {
-    $newModel = validation($_POST['Model']);
 
+require_once 'functions.php';
+
+if (isset($_POST['Brand'])){
+    if ($_POST ['Brand'] == TRUE && strlen ($_POST['Brand'] <= 255) && is_string($_POST['Brand'])){
+        $newBrand = validation($_POST['Brand']);
+    }
 }
-if (!empty($_POST['Year'])&& strlen( $_POST[ 'Year' ] ) === 4 ) && is_int($_POST[ 'Brand' ] )) {
+if (isset($_POST['Model'])){
+    if ($_POST['Model'] == TRUE && strlen($_POST['Model'] <= 255) && is_string($_POST['Model'])){
+        $newModel= validation($_POST['Model']);
+    }
+}
+if (isset($_POST['Year'])){
     $newYear = validation($_POST['Year']);
 }
+
+if (isset($newBrand) && isset($newModel) && isset($newYear) ){
+    $db = dbConn();
+    addNew($newBrand,$newModel,$newYear,$db);
+    header("Location: index.php");
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -23,13 +33,14 @@ if (!empty($_POST['Year'])&& strlen( $_POST[ 'Year' ] ) === 4 ) && is_int($_POST
 </head>
 <body>
 <h1>Add to your Collection</h1>
-<form action="index.php" method="post">
+<form action="insertToDb.php" method="post">
     <fieldset class="centerField">
     <div class="newBrand">Brand: <input type="text" placeholder="Enter the Brand" name="Brand" required></div>
     <div class="newModel">Model: <input type="text" placeholder="Enter the Model" name="Model" required></div>
     <div class="newYear">Year: <input type="text" placeholder="Enter the Year" name="Year" required></div>
         <div class="submitButton"><input type="submit"></div>
     </fieldset>
+    <a href="index.php">Home</a>
 </form>
 </body>
 </html>
